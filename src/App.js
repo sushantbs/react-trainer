9b556ce5c5d186be5cc722537036e66d3de7db4b
Sushant Badangod <sushant.bs@gmail.com>
2018-12-15 08:38:05 +0800

    Added the basic setup with mui

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import MenuIcon from "@material-ui/icons/Menu";
+import MuiRoot from "./withMui";
+
 import "./App.css";
 import IndexPage from "./routes/Index";
 import ProfilePage from "./routes/Profile";
+import {
+  withStyles,
+  IconButton,
+  AppBar,
+  Toolbar,
+  Typography,
+  Drawer
+} from "@material-ui/core";
+
+const styles = theme => {
+  return {
+    root: {}
+  };
+};
 
 class App extends Component {
+  state = {
+    drawerOpened: false
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">React Training</header>
+        <IconButton
+          className="menu-button"
+          onClick={e => this.setState({ drawerOpened: true })}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Drawer
+          variant="temporary"
+          open={this.state.drawerOpened}
+          onClose={e => this.setState({ drawerOpened: false })}
+        >
+          <Typography variant="h6">React Training</Typography>
+          <div>This is the drawer</div>
+        </Drawer>
         <BrowserRouter>
-          <div>
+          <div className="content">
             <Route path="/profile" component={ProfilePage} />
             <Route path="/" exact component={IndexPage} />
           </div>
@@ -20,4 +54,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withStyles(styles)(MuiRoot(App));
--- src/routes/Index.js
+++ src/routes/Index.js
@@ -1,5 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
+import Smiley from "@material-ui/icons/SentimentSatisfiedAlt";
+import { withStyles } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
 
-export default function(props) {
-  return <div>Index Route</div>;
+const styles = theme => ({
+  form: {
+    background: theme.palette.secondary.light,
+    padding: 40,
+    borderRadius: 5,
+    width: 350
+  },
+  formInput: {
+    marginBottom: 20,
+    marginTop: 20
+  }
+});
+
+class SignupPage extends Component {
+  state = {
+    username: "",
+    password: ""
+  };
+
+  _changeStateVar(stateVar, val) {
+    this.setState({
+      [stateVar]: val
+    });
+  }
+
+  render() {
+    let { classes } = this.props;
+    return (
+      <div className={classes.form}>
+        <Smiley />
+        <div> This is the signup page</div>
+        <TextField
+          className={classes.formInput}
+          fullWidth={true}
+          InputProps={{
+            value: this.state.username,
+            placeholder: "Username",
+            onChange: e => this._changeStateVar("username", e.target.value)
+          }}
+        />
+
+        <TextField
+          className={classes.formInput}
+          fullWidth={true}
+          InputProps={{
+            value: this.state.password,
+            placeholder: "Password",
+            onChange: e => this._changeStateVar("password", e.target.value)
+          }}
+        />
+      </div>
+    );
+  }
 }
+
+export default withStyles(styles)(SignupPage);
